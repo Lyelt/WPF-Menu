a7d9edbc567f394a36aece95be01d765ac7eb241
Nicholas Ghobrial <nicholasghob@yahoo.com>
2017-07-29 11:57:03 -0400

    Add project files.

---
+++ WPFmenu/Menu.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace Menu
+{
+    public class Menu
+    {
+        public string Name { get; set; }
+        public string MenuFile { get; set; }
+        public List<MenuItem> MenuItems { get; set; }
+        public Menu ()
+        {
+            Name = "New Menu";
+            MenuItems = new List<MenuItem>();
+        }
+
+        public Menu ( string fileName )
+        {
+            int fileNameStart = fileName.LastIndexOf("\\") + 1;
+            int length = fileName.LastIndexOf(".") - fileNameStart;
+
+            Name = fileName.Substring(fileNameStart, length);
+            MenuFile = fileName;
+            MenuItems = PopulateFromXML();
+        }
+
+        public MenuItem GetItem(string itemName)
+        {
+            // Get menu item if it exists
+            MenuItem item = null;
+            foreach (MenuItem mi in MenuItems)
+            {
+                if (mi.ItemName == itemName)
+                {
+                    item = mi;
+                }
+            }
+            
+            return item;
+        }
+
+        public MenuItem AddItem(string name, List<string> ingredients, List<string> types, List<string> meals, string eth)
+        {
+            if (!MenuItems.Contains(GetItem(name)))
+            {
+                MenuItems.Add(new MenuItem(name, ingredients, types, meals, eth));
+                // Return the newly added item
+                return MenuItems[MenuItems.Count - 1];
+            }    
+            else
+            {
+                return null;
+                // Update/Edit the item?
+            }
+
+        }
+
+        public List<MenuItem> PopulateFromXML()
+        {
+            var menuItems = new List<MenuItem>();
+            XmlDocument xmlDoc = new XmlDocument();
+            try
+            {
+                xmlDoc.Load(MenuFile);
+            }
+            catch
+            {
+                // Error loading xml document
+            }
+            
+            var itemNodes = xmlDoc.GetElementsByTagName("item");
+
+            foreach (XmlNode itemNode in itemNodes)
+            {
+                // -----Unique to all menu items-----------
+                string name = String.Empty;
+                List<string> ings = new List<string>();
+                var types = new List<MenuItem.FoodType>();
+                var meals = new List<MenuItem.Meal>();
+                var eth = MenuItem.Ethnicity.None;   
+                // ----------------------------------------
+                var childNodes = itemNode.ChildNodes;
+                foreach (XmlNode childNode in childNodes)
+                {
+                    switch (childNode.Name.ToLower())
+                    {
+                        // The name of the menu item
+                        case ("name"):
+                            name = childNode.InnerText;
+                            break;
+                        // The list of ingredients
+                        case ("ingredients"):
+                            var ingNodes = childNode.SelectNodes("li");
+                            foreach (XmlNode ingNode in ingNodes)
+                            {
+                                ings.Add(ingNode.InnerText);
+                            }
+                            break;
+                        // The type of food
+                        case ("types"):
+                            var typeNodes = childNode.SelectNodes("li");
+                            foreach (XmlNode typeNode in typeNodes)
+                            {
+                                MenuItem.FoodType ft;
+                                Enum.TryParse(typeNode.InnerText, out ft);
+                                types.Add(ft);
+                            }
+                            break;
+                        // The meal 
+                        case ("meals"):
+                            var mealNodes = childNode.SelectNodes("li");
+                            foreach (XmlNode mealNode in mealNodes)
+                            {
+                                MenuItem.Meal m;
+                                Enum.TryParse(mealNode.InnerText, out m);
+                                meals.Add(m);
+                            }
+                            break;
+                        // The ethnicity of the dish
+                        case ("ethnicity"):
+                            Enum.TryParse(childNode.InnerText, out eth);
+                            break;
+
+                        default:
+                            // Error
+                            break;
+                    }
+                }
+
+                menuItems.Add(new MenuItem(name, ings, types, meals, eth));
+            }
+
+            return menuItems;
+        }
+
+        public void Save( string fileName )
+        { 
+            var xml = new XElement("items",
+                MenuItems.Select(menuItem =>
+                    new XElement("item",
+                        new XElement("name", menuItem.ItemName),
+                        new XElement("ingredients", menuItem.Ingredients.Select(ing =>
+                            new XElement("li", ing))),
+                        new XElement("types", menuItem.ItemTypes.Select(type =>
+                            new XElement("li", type.ToString()))),
+                        new XElement("meals", menuItem.ItemMeals.Select(meal =>
+                            new XElement("li", meal.ToString()))),
+                        new XElement("ethnicity", menuItem.ItemEthnicity.ToString())
+                        ))
+               );
+
+            xml.Save(fileName);
+            //XmlDocument doc = new XmlDocument();
+
+        }
+    }
+}
---
+++ WPFmenu/MenuItem.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace Menu
+{
+    public class MenuItem
+    {
+        public enum FoodType
+        {
+            None,
+            Misc,
+            Chicken,
+            Pasta,
+            Rice,
+            Snack,
+            Dessert
+        }
+
+        public enum Ethnicity
+        {
+            None,
+            American,
+            Italian,
+            Asian,
+            Chinese,
+            Japanese,
+            Thai,
+            Indian,
+            Mexican,
+            Greek,
+            French
+        }
+
+        public enum Meal
+        {
+            Any,
+            None,
+            Breakfast,
+            Lunch,
+            Dinner,
+            Snack,
+            Dessert
+        }
+
+        public string ItemName;
+        public List<string> Ingredients;
+        public List<FoodType> ItemTypes;
+        public List<Meal> ItemMeals;
+        public Ethnicity ItemEthnicity;
+        public DateTime TimeAdded;
+
+        public MenuItem( string name, List<string> ings, List<FoodType> types, List<Meal> meals, Ethnicity ethnicity )
+        {
+            TimeAdded = DateTime.Now;
+            ItemName = name;
+            ItemEthnicity = ethnicity;
+            // Initialize food types
+            if (types != null)
+                ItemTypes = new List<FoodType>(types);
+            else
+            {
+                ItemTypes = new List<FoodType>();
+                ItemTypes.Add(FoodType.Misc);
+            }
+
+            // Initialize ingredients
+            if (ings != null)
+                Ingredients = new List<string>(ings);
+            else
+                Ingredients = new List<string>();
+
+            // Initialize meals
+            if (meals != null)
+                ItemMeals = new List<Meal>(meals);
+            else
+            {
+                ItemMeals = new List<Meal>();
+                ItemMeals.Add(Meal.None);
+            }    
+        }
+
+        public MenuItem( string name, List<string> ings, List<string> types, List<string> meals, string ethnicity )
+        {
+            ItemName = name;
+            if (!Enum.TryParse(ethnicity, out ItemEthnicity))
+                ItemEthnicity = Ethnicity.None;
+            if (ings != null)
+                Ingredients = new List<string>(ings);
+            else
+                Ingredients = new List<string>();
+
+            ItemTypes = new List<FoodType>();
+            foreach (var type in types)
+            {
+                FoodType ft;
+                if (!Enum.TryParse(type, out ft))
+                    ft = FoodType.Misc;
+                ItemTypes.Add(ft);
+            }
+
+            ItemMeals = new List<Meal>();
+            foreach (var meal in meals)
+            {
+                Meal m;
+                if (!Enum.TryParse(meal, out m))
+                    m = Meal.None;
+                ItemMeals.Add(m);
+            }
+        }
+
+        public List<string> ItemMeals_Strings()
+        {
+            var returnList = new List<string>();
+            foreach (var item in ItemMeals)
+            {
+                returnList.Add(item.ToString());
+            }
+            return returnList;
+        }
+
+        public List<string> ItemTypes_Strings()
+        {
+            var returnList = new List<string>();
+            foreach (var item in ItemTypes)
+            {
+                returnList.Add(item.ToString());
+            }
+            return returnList;
+        }
+
+    }
+}
